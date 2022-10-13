7bc51fc4f4f5c5e9538dea2cba2370378cadd0bb
nporto <88731950+nporto@users.noreply.github.com>
2022-10-13 11:57:31 -0300

    .

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,30 +1,64 @@
-import {  Box, Heading, Text, Image, AspectRatio, Container } from '@chakra-ui/react'
-import Mail from '../../Images/mail.png'
-import Map from '../../Images/map.png'
+import { Box, Heading, Text, Image, AspectRatio, Container } from "@chakra-ui/react";
+import Mail from "../../Images/mail.png";
+import Map from "../../Images/map.png";
 
 function Contact() {
-
-  return (
-   <Box background="rgb(249, 248, 255)" padding={{base: '0', md: '1.5rem', lg: '3rem'}} paddingTop={{base: '2rem', md: '', lg: ''}} textAlign="center" width="100vw">
-      <Heading>¿QUERÉS SUMARTE COMO SPONSOR?</Heading>
-      <Text>Comunicate con nosotros y te contamos como podes formar parte del evento</Text>
-      <Box display="flex" flexDirection={{base: 'column', md: 'column', lg: 'row'}} width="100%" justifyContent="center" padding="3rem">
-            <Box display="flex" flexDirection="column" padding="3rem" backgroundColor="white" borderRadius="0.6rem" alignItems='center'>
-                  <Image src={Map} maxWidth="48px" />
-                  <Heading padding="1rem" size="lg">SEDE DEL EVENTO</Heading>
-                  <Text>ITBA Sede Distrito Financiero, San Martín 202</Text>
+   return (
+      <Box
+         background="rgb(249, 248, 255)"
+         padding={{ base: "0", md: "1.5rem", lg: "3rem" }}
+         paddingTop={{ base: "2rem", md: "", lg: "" }}
+         textAlign="center"
+         width="100vw"
+      >
+         <Heading>¿QUERÉS SUMARTE COMO SPONSOR?</Heading>
+         <Text>Comunicate con nosotros y te contamos como podes formar parte del evento</Text>
+         <Box
+            display="flex"
+            flexDirection={{ base: "column", md: "column", lg: "row" }}
+            width="100%"
+            justifyContent="center"
+            padding="3rem"
+         >
+            <Box
+               display="flex"
+               flexDirection="column"
+               padding="3rem"
+               backgroundColor="white"
+               borderRadius="0.6rem"
+               alignItems="center"
+            >
+               <Image src={Map} maxWidth="48px" />
+               <Heading padding="1rem" size="lg">
+                  SEDE DEL EVENTO
+               </Heading>
+               <Text>ITBA Sede Distrito Financiero, San Martín 202</Text>
             </Box>
-            <Box display="flex" flexDirection="column" padding="3rem" backgroundColor="white" borderRadius="0.6rem" marginX={{base: '0rem', md: '0rem', lg: '3rem'}} marginY={{base: '2rem', md: '2.5rem', lg: '0rem'}} alignItems='center'>
-                  <Image src={Mail} maxWidth="48px" />
-                  <Heading padding="1rem" size="lg">Contactános</Heading>
-                  <Text maxWidth="100%">E-Mail</Text>
+            <Box
+               display="flex"
+               flexDirection="column"
+               padding="3rem"
+               backgroundColor="white"
+               borderRadius="0.6rem"
+               marginX={{ base: "0rem", md: "0rem", lg: "3rem" }}
+               marginY={{ base: "2rem", md: "2.5rem", lg: "0rem" }}
+               alignItems="center"
+            >
+               <Image src={Mail} maxWidth="48px" />
+               <Heading padding="1rem" size="lg">
+                  Contactános
+               </Heading>
+               <Text maxWidth="100%">emprendedores@itba.edu.ar</Text>
             </Box>
+         </Box>
+         <Box display="flex" flexWrap="wrap" boxSizing="border-box" justifyContent="center" paddingBottom="2rem">
+            <iframe
+               src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1952.6453721482706!2d-58.37497404314368!3d-34.6055525805552!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd8c7176f8bd13237!2sITBA%20Sede%20Distrito%20Financiero!5e0!3m2!1ses-419!2sar!4v1661465186687!5m2!1ses-419!2sar"
+               style={{ border: "0", width: "80%", height: "450px", objectFit: "cover" }}
+            ></iframe>
+         </Box>
       </Box>
-      <Box display="flex" flexWrap="wrap" boxSizing="border-box"  justifyContent="center" paddingBottom="2rem">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1952.6453721482706!2d-58.37497404314368!3d-34.6055525805552!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd8c7176f8bd13237!2sITBA%20Sede%20Distrito%20Financiero!5e0!3m2!1ses-419!2sar!4v1661465186687!5m2!1ses-419!2sar" style={{border: '0', width: '80%', height: '450px', objectFit: 'cover'}}></iframe>
-      </Box>
-   </Box>
-  )
+   );
 }
 
-export default Contact
+export default Contact;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,58 +1,72 @@
-import {  UnorderedList, ListItem, Box, Button, Heading, Text, Image } from '@chakra-ui/react'
-import Info from './Info'
-import logoStandard from '../../Images/logoStandard.png'
-import QR2 from '../../Images/QR2.jpeg'
-import Twitter1 from '../../Images/Twitter32.png'
-import Linkedin from '../../Images/Linkedin32.png'
-import logoITBAWhite from '../../Images/logoITBAWhite.png'
-import logoITBA from '../../Images/ITBA-logo.png'
+import { UnorderedList, ListItem, Box, Button, Heading, Text, Image } from "@chakra-ui/react";
+import Info from "./Info";
+import logoStandard from "../../Images/logoStandard.png";
+import QR2 from "../../Images/QR2.jpeg";
+import Twitter1 from "../../Images/Twitter32.png";
+import Linkedin from "../../Images/Linkedin32.png";
+import logoITBAWhite from "../../Images/logoITBAWhite.png";
+import logoITBA from "../../Images/ITBA-logo.png";
 
 function Home() {
+   // background linear-gradient(45deg, rgb(17, 33, 73) 0%, rgb(19, 81, 239) 100%), url(../Images/hero-bg.jpg) center center no-repeat;
 
-// background linear-gradient(45deg, rgb(17, 33, 73) 0%, rgb(19, 81, 239) 100%), url(../Images/hero-bg.jpg) center center no-repeat;
-
-  return (
-   <Box 
-   id="home"
-   minHeight={{base: '90vh', md: '85vh', lg: '87vh'}}
-   maxWidth="100vw"
-   display="flex" 
-   flexDirection={{base: "column-reverse", md: "row", lg: "row"}} 
-   alignItems="center" 
-   paddingLeft={{base: '0', md: '0', lg: '10rem'}}
-   background="linear-gradient(45deg, rgb(51, 63, 255) 0%, rgb(71, 222, 242) 100%), url(../Images/hero-bg.jpg) center center no-repeat;"
-   textAlign={{base: 'center', md: 'inherit', lg: 'inherit'}}>
-         <Box padding="5rem" paddingTop={{base: '2rem', md: '5rem', lg: '5rem'}} width={{base: '100%', md: '70%', lg: '50%'}}>
-            <Text marginY={{base: '1rem', md: '1rem', lg: '1.1rem'}} color="green.400" fontWeight="700" fontSize="1.6rem">Fecha | Lugar</Text>
-            <Heading color="white" size="4xl"><Image src={logoStandard} /></Heading>
-            <Text marginY={{base: '1rem', md: '1rem', lg: '1.1rem'}} color="white" fontSize="1.3rem">Slogan</Text>
-            <Button paddingTop="1.6rem" colorScheme="green" padding="1.6rem" fontSize="1.5rem"><a href="#team">Get Started</a></Button>
+   return (
+      <Box
+         id="home"
+         minHeight={{ base: "90vh", md: "85vh", lg: "87vh" }}
+         maxWidth="100vw"
+         display="flex"
+         flexDirection={{ base: "column-reverse", md: "row", lg: "row" }}
+         alignItems="center"
+         paddingLeft={{ base: "0", md: "0", lg: "10rem" }}
+         background="linear-gradient(45deg, rgb(51, 63, 255) 0%, rgb(71, 222, 242) 100%), url(../Images/hero-bg.jpg) center center no-repeat;"
+         textAlign={{ base: "center", md: "inherit", lg: "inherit" }}
+      >
+         <Box
+            padding="5rem"
+            paddingTop={{ base: "2rem", md: "5rem", lg: "5rem" }}
+            width={{ base: "100%", md: "70%", lg: "50%" }}
+         >
+            <Text
+               marginY={{ base: "1rem", md: "1rem", lg: "1.1rem" }}
+               color="green.400"
+               fontWeight="700"
+               fontSize="1.6rem"
+            >
+               Fecha | Lugar
+            </Text>
+            <Heading color="white" size="4xl">
+               <Image src={logoStandard} />
+            </Heading>
+            <Text marginY={{ base: "1rem", md: "1rem", lg: "1.1rem" }} color="white" fontSize="1.3rem">
+               Slogan
+            </Text>
+            <Button paddingTop="1.6rem" colorScheme="green" padding="1.6rem" fontSize="1.5rem">
+               <a href="#team">Get Started</a>
+            </Button>
          </Box>
-         <Box 
-         paddingTop={{base: "auto", md: '3rem', lg: 'auto'}} 
-         marginRight={{base: "0%", md: 'auto', lg: 'auto'}} 
-         marginTop={{base: '25%', md: 'auto', lg: 'auto'}} 
-         marginLeft={{base: '50%', md: 'auto', lg: 'auto'}} 
-         marginBottom="auto" 
-         width={{base: '100%', md: '55%', lg: '50%'}}>
-            <Image 
-            maxWidth={{base: '50%', md: '260px', lg: '320px'}}
-            height="auto" 
-            objectFit="cover" 
-            src={QR2} />
+         <Box
+            paddingTop={{ base: "auto", md: "3rem", lg: "auto" }}
+            marginRight={{ base: "0%", md: "auto", lg: "auto" }}
+            marginTop={{ base: "25%", md: "auto", lg: "auto" }}
+            marginLeft={{ base: "50%", md: "auto", lg: "auto" }}
+            marginBottom="auto"
+            width={{ base: "100%", md: "55%", lg: "50%" }}
+         >
+            <Image maxWidth={{ base: "50%", md: "260px", lg: "320px" }} height="auto" objectFit="cover" src={QR2} />
          </Box>
-   </Box>
-  )
+      </Box>
+   );
 }
 
-export default Home
+export default Home;
 
-//maxWidth="50%" 
+//maxWidth="50%"
 
 //<Box display="flex"  alignItems="center" paddingLeft="3rem">
-          //     <UnorderedList position="relative" display="flex" flexDirection="row" styleType="none" alignItems="center" justifyContent="space-around">
-                   //     <ListItem px="0.8rem" color="white" cursor="pointer"><a href="https://twitter.com/ITBAW3LAB" target="_blank"><Image src={Twitter1} /></a></ListItem>
-                   //     <ListItem px="0.8rem" color="white" cursor="pointer"><a href="https://www.linkedin.com/company/itba-w3-lab/?viewAsMember=true" target="_blank"><Image src={Linkedin} /></a></ListItem>
-                   //     <ListItem px="0.8rem" color="white" cursor="pointer"><a href="https://www.itba.edu.ar/" target="_blank"><Image maxHeight="48px" src={logoITBAWhite} /></a></ListItem>
-              // </UnorderedList>
-        // </Box>
+//     <UnorderedList position="relative" display="flex" flexDirection="row" styleType="none" alignItems="center" justifyContent="space-around">
+//     <ListItem px="0.8rem" color="white" cursor="pointer"><a href="https://twitter.com/ITBAW3LAB" target="_blank"><Image src={Twitter1} /></a></ListItem>
+//     <ListItem px="0.8rem" color="white" cursor="pointer"><a href="https://www.linkedin.com/company/itba-w3-lab/?viewAsMember=true" target="_blank"><Image src={Linkedin} /></a></ListItem>
+//     <ListItem px="0.8rem" color="white" cursor="pointer"><a href="https://www.itba.edu.ar/" target="_blank"><Image maxHeight="48px" src={logoITBAWhite} /></a></ListItem>
+// </UnorderedList>
+// </Box>
